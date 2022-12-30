39373545f5f694a394f8c41c1d13e38b3d815d57
Yann <yann.barbier@ens.uvsq.fr>
2022-12-30 19:27:11 +0100

    Résolution d'un problème de désynchronisation entre les deux joueurs

--- datasetGenerator.py
+++ datasetGenerator.py
@@ -21,7 +21,7 @@ def generate_dataset(indir, outfile):
     with open(outfile, "w", newline = "") as csv_file:
     
         writer = csv.writer(csv_file)
-        writer.writerow(["time", "race1", "race2", "supplyMade1", "supplyMade2", "supplyUsed1", "supplyUsed2", "totalIncome1", "totalIncome2", "mineralsIncome1", "mineralsIncome2", "vespeneIncome1", "vespeneIncome2", "totalResources1", "totalResources2", "minerals1", "minerals2", "vespene1", "vespene2", "activeWorkers1", "activeWorkers2", "army1", "army2", "technology1", "technology2", "lostResources1", "lostResources2", "winner"])
+        writer.writerow(["time", "race1", "race2", "supplyUsed1", "supplyUsed2", "supplyMade1", "supplyMade2", "totalIncome1", "totalIncome2", "mineralsIncome1", "mineralsIncome2", "vespeneIncome1", "vespeneIncome2", "totalResources1", "totalResources2", "minerals1", "minerals2", "vespene1", "vespene2", "activeWorkers1", "activeWorkers2", "army1", "army2", "technology1", "technology2", "lostResources1", "lostResources2", "winner"])
 
         for filename in [f for f in listdir(indir) if f.endswith(".SC2Replay")]:
             print("Extracting from", filename, "...")
@@ -31,10 +31,10 @@ def generate_dataset(indir, outfile):
             race1 = race_to_id(replay.players[0].play_race)
             race2 = race_to_id(replay.players[1].play_race)
 
-            supplyMade1 = 0
-            supplyMade2 = 0
             supplyUsed1 = 0
             supplyUsed2 = 0
+            supplyMade1 = 0
+            supplyMade2 = 0
             totalIncome1 = 0
             totalIncome2 = 0
             mineralsIncome1 = 0
@@ -63,11 +63,13 @@ def generate_dataset(indir, outfile):
             last_measurement_frame = 0
             frame_interval = 112
 
+            # FIX LE DELAI DE J2
+
             for event in replay.events:
                 if isinstance(event, PlayerStatsEvent):
                     if event.pid == 1:
-                        supplyMade1 = event.food_made
                         supplyUsed1 = event.food_used
+                        supplyMade1 = event.food_made
                         totalIncome1 = event.minerals_collection_rate + event.vespene_collection_rate
                         mineralsIncome1 = event.minerals_collection_rate
                         vespeneIncome1 = event.vespene_collection_rate
@@ -80,8 +82,8 @@ def generate_dataset(indir, outfile):
                         lostResources1 = event.resources_lost
 
                     elif event.pid == 2:
-                        supplyMade2 = event.food_made
                         supplyUsed2 = event.food_used
+                        supplyMade2 = event.food_made
                         totalIncome2 = event.minerals_collection_rate + event.vespene_collection_rate
                         mineralsIncome2 = event.minerals_collection_rate
                         vespeneIncome2 = event.vespene_collection_rate
@@ -92,16 +94,16 @@ def generate_dataset(indir, outfile):
                         army2 = event.minerals_used_current_army + event.vespene_used_current_army
                         technology2 = event.minerals_used_current_technology + event.vespene_used_current_technology
                         lostResources2 = event.resources_lost
+
+                        if frame >= (last_measurement_frame + frame_interval):
+                            time = frame_to_realtime(frame)
+                            writer.writerow([time, race1, race2, supplyUsed1, supplyUsed2, supplyMade1, supplyMade2, totalIncome1, totalIncome2, mineralsIncome1, mineralsIncome2, vespeneIncome1, vespeneIncome2, totalResources1, totalResources2, minerals1, minerals2, vespene1, vespene2, activeWorkers1, activeWorkers2, army1, army2, technology1, technology2, lostResources1, lostResources2, winner])
+                            last_measurement_frame = frame
                     
                     frame = event.frame
 
                 elif isinstance(event, PlayerLeaveEvent):
                     break
-
-                if frame >= (last_measurement_frame + frame_interval):
-                    time = frame_to_realtime(frame)
-                    writer.writerow([time, race1, race2, supplyMade1, supplyMade2, supplyUsed1, supplyUsed2, totalIncome1, totalIncome2, mineralsIncome1, mineralsIncome2, vespeneIncome1, vespeneIncome2, totalResources1, totalResources2, minerals1, minerals2, vespene1, vespene2, activeWorkers1, activeWorkers2, army1, army2, technology1, technology2, lostResources1, lostResources2, winner])
-                    last_measurement_frame = frame
 
 generate_dataset("replays/trainSet", "generatedDatasets/trainSet.csv")
 generate_dataset("replays/testSet", "generatedDatasets/testSet.csv")
