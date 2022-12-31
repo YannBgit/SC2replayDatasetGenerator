e4b4038178afdb82383a2f4f9103bddf07cc815e
Yann <yann.barbier@ens.uvsq.fr>
2022-12-31 02:45:32 +0100

    Ajout d'une vérification que la partie soit bien un 1v1

--- datasetGenerator.py
+++ datasetGenerator.py
@@ -27,83 +27,84 @@ def generate_dataset(indir, outfile):
             print("Extracting from", filename, "...")
             replay = sc2reader.load_replay(path.join(indir, filename))
 
-            frame = 0
-            race1 = race_to_id(replay.players[0].play_race)
-            race2 = race_to_id(replay.players[1].play_race)
+            if len(replay.players) == 2:
+                frame = 0
+                race1 = race_to_id(replay.players[0].play_race)
+                race2 = race_to_id(replay.players[1].play_race)
 
-            supplyUsed1 = 0
-            supplyUsed2 = 0
-            supplyMade1 = 0
-            supplyMade2 = 0
-            totalIncome1 = 0
-            totalIncome2 = 0
-            mineralsIncome1 = 0
-            mineralsIncome2 = 0
-            vespeneIncome1 = 0
-            vespeneIncome2 = 0
-            totalResources1 = 0
-            totalResources2 = 0
-            minerals1 = 0
-            minerals2 = 0
-            vespene1 = 0
-            vespene2 = 0
-            activeWorkers1 = 0
-            activeWorkers2 = 0
-            army1 = 0
-            army2 = 0
-            technology1 = 0
-            technology2 = 0
-            lostResources1 = 0
-            lostResources2 = 0
+                supplyUsed1 = 0
+                supplyUsed2 = 0
+                supplyMade1 = 0
+                supplyMade2 = 0
+                totalIncome1 = 0
+                totalIncome2 = 0
+                mineralsIncome1 = 0
+                mineralsIncome2 = 0
+                vespeneIncome1 = 0
+                vespeneIncome2 = 0
+                totalResources1 = 0
+                totalResources2 = 0
+                minerals1 = 0
+                minerals2 = 0
+                vespene1 = 0
+                vespene2 = 0
+                activeWorkers1 = 0
+                activeWorkers2 = 0
+                army1 = 0
+                army2 = 0
+                technology1 = 0
+                technology2 = 0
+                lostResources1 = 0
+                lostResources2 = 0
 
-            for player in replay.players:
-                if player.result == "Win":
-                    winner = player.pid - 1
-            
-            last_measurement_frame = 0
-            frame_interval = 112
+                for player in replay.players:
+                    if player.result == "Win":
+                        winner = player.pid - 1
+                
+                last_measurement_frame = 0
+                frame_interval = 112
 
-            # FIX LE DELAI DE J2
+                # FIX LE DELAI DE J2
 
-            for event in replay.events:
-                if isinstance(event, PlayerStatsEvent):
-                    if event.pid == 1:
-                        supplyUsed1 = event.food_used
-                        supplyMade1 = event.food_made
-                        totalIncome1 = event.minerals_collection_rate + event.vespene_collection_rate
-                        mineralsIncome1 = event.minerals_collection_rate
-                        vespeneIncome1 = event.vespene_collection_rate
-                        totalResources1 = event.minerals_current + event.vespene_current
-                        minerals1 = event.minerals_current
-                        vespene1 = event.vespene_current
-                        activeWorkers1 = event.workers_active_count
-                        army1 = event.minerals_used_current_army + event.vespene_used_current_army
-                        technology1 = event.minerals_used_current_technology + event.vespene_used_current_technology
-                        lostResources1 = event.resources_lost
+                for event in replay.events:
+                    if isinstance(event, PlayerStatsEvent):
+                        if event.pid == 1:
+                            supplyUsed1 = event.food_used
+                            supplyMade1 = event.food_made
+                            totalIncome1 = event.minerals_collection_rate + event.vespene_collection_rate
+                            mineralsIncome1 = event.minerals_collection_rate
+                            vespeneIncome1 = event.vespene_collection_rate
+                            totalResources1 = event.minerals_current + event.vespene_current
+                            minerals1 = event.minerals_current
+                            vespene1 = event.vespene_current
+                            activeWorkers1 = event.workers_active_count
+                            army1 = event.minerals_used_current_army + event.vespene_used_current_army
+                            technology1 = event.minerals_used_current_technology + event.vespene_used_current_technology
+                            lostResources1 = event.resources_lost
 
-                    elif event.pid == 2:
-                        supplyUsed2 = event.food_used
-                        supplyMade2 = event.food_made
-                        totalIncome2 = event.minerals_collection_rate + event.vespene_collection_rate
-                        mineralsIncome2 = event.minerals_collection_rate
-                        vespeneIncome2 = event.vespene_collection_rate
-                        totalResources2 = event.minerals_current + event.vespene_current
-                        minerals2 = event.minerals_current
-                        vespene2 = event.vespene_current
-                        activeWorkers2 = event.workers_active_count
-                        army2 = event.minerals_used_current_army + event.vespene_used_current_army
-                        technology2 = event.minerals_used_current_technology + event.vespene_used_current_technology
-                        lostResources2 = event.resources_lost
+                        elif event.pid == 2:
+                            supplyUsed2 = event.food_used
+                            supplyMade2 = event.food_made
+                            totalIncome2 = event.minerals_collection_rate + event.vespene_collection_rate
+                            mineralsIncome2 = event.minerals_collection_rate
+                            vespeneIncome2 = event.vespene_collection_rate
+                            totalResources2 = event.minerals_current + event.vespene_current
+                            minerals2 = event.minerals_current
+                            vespene2 = event.vespene_current
+                            activeWorkers2 = event.workers_active_count
+                            army2 = event.minerals_used_current_army + event.vespene_used_current_army
+                            technology2 = event.minerals_used_current_technology + event.vespene_used_current_technology
+                            lostResources2 = event.resources_lost
 
-                        if frame >= (last_measurement_frame + frame_interval):
-                            time = frame_to_realtime(frame)
-                            writer.writerow([time, race1, race2, supplyUsed1, supplyUsed2, supplyMade1, supplyMade2, totalIncome1, totalIncome2, mineralsIncome1, mineralsIncome2, vespeneIncome1, vespeneIncome2, totalResources1, totalResources2, minerals1, minerals2, vespene1, vespene2, activeWorkers1, activeWorkers2, army1, army2, technology1, technology2, lostResources1, lostResources2, winner])
-                            last_measurement_frame = frame
-                    
-                    frame = event.frame
+                            if frame >= (last_measurement_frame + frame_interval):
+                                time = frame_to_realtime(frame)
+                                writer.writerow([time, race1, race2, supplyUsed1, supplyUsed2, supplyMade1, supplyMade2, totalIncome1, totalIncome2, mineralsIncome1, mineralsIncome2, vespeneIncome1, vespeneIncome2, totalResources1, totalResources2, minerals1, minerals2, vespene1, vespene2, activeWorkers1, activeWorkers2, army1, army2, technology1, technology2, lostResources1, lostResources2, winner])
+                                last_measurement_frame = frame
+                        
+                        frame = event.frame
 
-                elif isinstance(event, PlayerLeaveEvent):
-                    break
+                    elif isinstance(event, PlayerLeaveEvent):
+                        break
 
 generate_dataset("replays/trainSet", "generatedDatasets/trainSet.csv")
 generate_dataset("replays/testSet", "generatedDatasets/testSet.csv")
